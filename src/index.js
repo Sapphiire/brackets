66d4484cc74888901522ca45edba7cbc110e5075
Vadzim <Sapphiire@github.com>
2020-10-13 19:14:55 +0300

    v2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  const regexp = new RegExp('[' + bracketsConfig.reduce((accumulator, current) => accumulator + current.reduce((acc1,cur1, index) => {
-    const value = "[]\\/^$.|?*+(){}".split('').every(el => cur1.indexOf(el) === -1) ? cur1 : "\\" + cur1;
-    return index == 1 ? acc1 + ".*" + value : acc1 + value
-      
-  }, ""), "") + ']')
-  return regexp.test(str)
+  const length = str.length;
+  let brackets_open = new Map();
+  let brackets_exit = new Map();
+  let array = [];
+
+  bracketsConfig.forEach(element => brackets_open.set(element[0],element[1]))
+  bracketsConfig.forEach(element => brackets_exit.set(element[1],element[0]))
+
+  for(let i = 0; i < length; i++) {
+    if(brackets_exit.has(str[i])) {
+      if(!array.length || array[array.length - 1] != brackets_exit.get(str[i]))
+        return false;
+      else
+        array.pop();
+    }
+    else
+      array.push(str[i]);
+  }
+  return true
 }
