63698a934bd55297a2f9615fcf44c561e4f26e0d
Vadzim <Sapphiire@github.com>
2020-10-13 18:30:10 +0300

    v1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const regexp = new RegExp('[' + bracketsConfig.reduce((accumulator, current) => accumulator + current.reduce((acc1,cur1, index) => {
+    const value = "[]\\/^$.|?*+(){}".split('').every(el => cur1.indexOf(el) === -1) ? cur1 : "\\" + cur1;
+    return index == 1 ? acc1 + ".*" + value : acc1 + value
+      
+  }, ""), "") + ']')
+  return regexp.test(str)
 }
